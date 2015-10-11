e30c96d96c3cfff84fd698b727dac65b44293992
Kaspervos <KasperWoW@Live.nl>
2015-10-11 18:47:55 +0200

    Added a Spritehelperclass
    Method added: spritesheetloader

--- src/GameLogic/Sprite.java
+++ src/GameLogic/Sprite.java
@@ -1,5 +1,9 @@
 package GameLogic;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javafx.scene.image.Image;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -12,4 +16,37 @@ package GameLogic;
  */
 public class Sprite {
     
+    int width;
+    int height;
+    int rows;
+    int columns;
+    BufferedImage[] sprites;
+    
+    /**
+     * 
+     */
+    public Sprite() {
+        
+    }
+    
+    /**
+     * 
+     * @param w - width of the spritesheet
+     * @param h - height of the spritesheet
+     * @param r - amount of rows in the spritesheet
+     * @param c - amount of columns in the spritesheet
+     * @param buffImg - the loaded spritesheet
+     * @return
+     * @throws IOException 
+     */
+    public BufferedImage[] LoadSpriteSheet(int w, int h, int r, int c, BufferedImage buffImg) throws IOException {
+        sprites = new BufferedImage[r * c];
+        
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                sprites[(i * c) + j] = buffImg.getSubimage(i * w, j * h, w, h);
+            }
+        }
+        return sprites;
+    }
 }
