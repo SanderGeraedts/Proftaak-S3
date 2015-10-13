a9b289821fd87a61b1d60d07ecefda19d5684a72
HMikhaeljan <Hovsep_@hotmail.com>
2015-10-13 11:07:00 +0200

    Update on Ability and PlayerRole

--- src/GameLogic/Ability.java
+++ src/GameLogic/Ability.java
@@ -2,7 +2,6 @@ package GameLogic;
 
 public class Ability {
 
-	private int id;
 	private String name;
 	private int damage;
 	private String spriteID;
@@ -11,17 +10,39 @@ public class Ability {
 	/**
 	 * 
 	 * @param id
-	 * @param name
-	 * @param damage
-	 * @param spriteID
-	 * @param cooldownTimer
 	 */
-	public Ability(int id, String name, int damage, String spriteID, int cooldownTimer) {
-            this.id = id;
-            this.name = name;
-            this.damage = damage;
-            this.spriteID = spriteID;
-            this.cooldownTimer = cooldownTimer;
+	public Ability(int id) {
+            LoadAbility(id);
+        }
+        
+        private void LoadAbility(int id){
+            
+            switch(id){
+                //Rogue role
+                case 1:
+                    name = "Basic attack";
+                    damage = 10;
+                    spriteID = "1";
+                    cooldownTimer = 1;
+                    break;
+                case 2:
+                    name = "Combo breaker";
+                    damage = 30;
+                    spriteID = "2";
+                    cooldownTimer = 30;
+                    break;
+                case 3:
+                    name = "Poison Trap";
+                    damage = 15;
+                    spriteID = "3";
+                    cooldownTimer = 15;
+                    break;
+        }
+        }
+            
+        public String GetSkillInfo(){
+                String temp = "Name: "+name +"\nDamage: "+damage + "\nCooldown: " + cooldownTimer;
+                return temp;
         }
 
 }
