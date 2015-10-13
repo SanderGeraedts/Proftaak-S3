fa57f9c733115d5ae1975fe8ae3cd5be90affb40
RoyTimmers <roytimmers_7@hotmail.com>
2015-10-13 11:27:47 +0200

    User en Stat volledig gespecificeerd UnitTest aan begonnen

--- src/GameLogic/Stat.java
+++ src/GameLogic/Stat.java
@@ -1,30 +1,93 @@
 package GameLogic;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * Stat class used in User
+ * 
+ * @author Roy
+ */
 public class Stat {
 
-	private int statsID;
-	private int totalWins;
-	private int totalLosses;
-	private int totalKills;
-	private int totalDeaths;
-	private int rating;
+    private int statsID;
+    private int totalWins;
+    private int totalLosses;
+    private int totalKills;
+    private int totalDeaths;
+    private int rating;
+    
 
-	public Stat() {
-		// TODO - implement Stat.Stat
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * Constructor for Stat
+     * @param statsid must be identical to userID
+     * @param totalwins 
+     * @param totallosses
+     * @param totalkills
+     * @param totaldeaths
+     * @param rating
+     */
+    public Stat(int statsid, int totalwins, int totallosses, int totalkills, int totaldeaths, int rating) {
+        throw new NotImplementedException();
 
-        /**
-        *Give back the stats.
-        * @param totalWins
-        * @param totalLosses
-        * @param totalKills
-        * @param totalDeaths
-        * @param rating
-        */
-	public void requestStats() {
-		// TODO - implement Stat.requestStats
-		throw new UnsupportedOperationException();
-	}
+    }
 
+    public int getStatsID() {
+        return statsID;
+    }
+
+    public int getTotalWins() {
+        return totalWins;
+    }
+
+    public int getTotalLosses() {
+        return totalLosses;
+    }
+
+    public int getTotalKills() {
+        return totalKills;
+    }
+
+    public int getTotalDeaths() {
+        return totalDeaths;
+    }
+
+    /**
+     * Add kills
+     *
+     * @param kills how many players were killed.
+     */
+    public void addKills(int kills) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Add deaths
+     *
+     * @param deaths how many times player died.
+     */
+    public void addDeaths(int deaths) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Add wins or losses
+     *
+     * @param won if won is true one win is added otherwise one loss.
+     */
+    public void winOrLose(boolean won) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Give back the stats.
+     *
+     * @param totalWins
+     * @param totalLosses
+     * @param totalKills
+     * @param totalDeaths
+     * @param rating
+     */
+    public void requestStats() {
+        throw new NotImplementedException();
+    }
 }
--- src/GameLogic/User.java
+++ src/GameLogic/User.java
@@ -1,24 +1,72 @@
 package GameLogic;
 
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 public class User {
 
-	private int userID;
-	private String name;
-	private String password;
-	private String email;
+    private int userID;
+    private String name;
+    private String email;
+    private Stat stats;
 
-	/**
-	 * 
-	 * @param userID
-	 * @param name
-	 * @param password
-	 * @param email
-	 */
-	public User(int userID, String name, String password, String email) {
-		this.userID = userID;
-                this.name = name;
-                this.password = password;
-                this.email = email;
-	}
+    /**
+     * Constructor for User
+     *
+     * @param userID Cannot be null and must be unique.
+     * @param name Cannot be null and must be unique.
+     * @param email Cannot be null.
+     */
+    public User(int userID, String name, String email) {
+        throw new NotImplementedException();
+    }
 
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Login method for verifying user. Checks in database if name and password
+     * has correspondig account. if User exist then return user with
+     * corresponding stats
+     *
+     * @param name
+     * @param password
+     * @return User if account exists otherwise return null.
+     */
+    public User login(String name, String password) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Create account Creates an account in the database.
+     *
+     * @param userid cannot not be null and must be unique.
+     * @param name cannot not be null and must be unique.
+     * @param password cannot not be null.
+     * @param email cannot be null.
+     * @param stats cannot be null.
+     * @return true if succesfully added in database.
+     */
+    public Boolean createAccount(String userid, String name, String password, String email, Stat stats) {
+        throw new NotImplementedException();
+    }
+
+    /**
+     * Changes stats after a game ended.
+     *
+     * @param kills how many players were killed by player.
+     * @param deaths how many times player died.
+     * @param win if the game is won must be true, lost is false;
+     */
+    public void changeStats(int kills, int deaths, boolean win) {
+        throw new NotImplementedException();
+    }
 }
