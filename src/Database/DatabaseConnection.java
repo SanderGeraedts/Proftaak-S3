e7bf8944932495c263ba738bf509643c4302adb5
HMikhaeljan <Hovsep_@hotmail.com>
2015-10-30 22:33:10 +0100

    online db connection + lobby and login fxml
    Online database connection. heel misschien dat je zelf ff de
    mysql-connector-java.5.1.37-bin.jar moet toevoegen aan de librarys.

--- src/Database/DatabaseConnection.java
+++ src/Database/DatabaseConnection.java
@@ -5,28 +5,83 @@
  */
 package Database;
 
+import GameLogic.PlayerRole;
+import GameLogic.Stat;
+import GameLogic.User;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author Hovsep
  */
 public class DatabaseConnection {
+
     Connection conn;
-    
+
     public DatabaseConnection() {
-        String url = "jdbc:mysql:http://codepanda.nl/phpmyadmin/index.php?db=sanderge_PTS3&table=Stats&target=tbl_structure.php&token=0b3c6beec57be97199390638cccb4ccd#PMAURL:server=1&target=main.php&token=0b3c6beec57be97199390638cccb4ccd";
-        String driver = "com.mysql.jdbc.Driver";
-        String userName = "sanderge_PTS3";
-        String password = "ProftaakS34C";
+        String url = "jdbc:mysql://sql2.freesqldatabase.com:3306/";
+        String dbName = "sql294852";
+        String userName = "sql294852";
+        String password = "dA7%fV6*";
         try {
-            Class.forName(driver).newInstance();
-            conn = DriverManager.getConnection(url, userName, password);
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url + dbName, userName, password);
+            System.out.println("Connecting to database works");
         } catch (Exception e) {
+            System.out.println("Connecting to database failed");
             e.printStackTrace();
         }
     }
-    
-    
-    
+
+    //todo
+    public Stat getStat() {
+        return null;
+    }
+
+    //todo
+    public List<PlayerRole> getRole() {
+        return null;
+    }
+
+    //test
+    public String getUsername() throws SQLException {
+        Statement st = conn.createStatement();
+        ResultSet srs = st.executeQuery("SELECT * FROM User");
+        String username = "";
+        while (srs.next()) {
+            username = srs.getString("user_name");
+        }
+        return username;
+    }
+
+    public String getPassword() throws SQLException {
+        Statement st = conn.createStatement();
+        ResultSet srs = st.executeQuery("SELECT * FROM User");
+        String password = "";
+        while (srs.next()) {
+            password = srs.getString("user_password");
+        }
+        return password;
+    }
+
+    //todo
+    public List<User> getUsers() throws SQLException {
+        Statement st = conn.createStatement();
+        ResultSet srs = st.executeQuery("SELECT * FROM User");
+        ArrayList<User> userlist = new ArrayList<>();
+        String username = "";
+        while (srs.next()) {
+            int userid = srs.getInt("user_id");
+            username = srs.getString("user_name");
+            String password = srs.getString("user_password");
+
+            User usr = new User(username);
+            userlist.add(usr);
+        }
+        st.close();
+        return userlist;
+    }
+
 }
---
+++ src/Database/DatabaseReader.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Database;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Hovsep
+ */
+public class DatabaseReader {
+
+    public static void main(String[] args) throws SQLException {
+        DatabaseConnection db = new DatabaseConnection();
+        System.out.println("Username: "+db.getUsername() + "\nPassword: " + db.getPassword());
+
+    }
+}
