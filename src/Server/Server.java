d9d81facd76eb72e9c2741398e72f6a12924a305
jeroen0606 <jeroen0606@gmail.com>
2015-11-30 13:59:10 +0100

    Added code to client and added fontys observer

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -5,10 +5,104 @@
  */
 package Server;
 
+import fontys.observer.BasicPublisher;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Enumeration;
+
 /**
  *
  * @author Jeroen0606
  */
 public class Server {
+    // Set port number
+    private static final int portNumber = 1099;
+
+    // Set binding name for mEFB
+    private static final String bindingName = "AEXBanner";
+
+    // References to registry and mEFB
+    private Registry registry = null;
     
+    private BasicPublisher publisher;
+
+    // Constructor
+    public Server() {
+        
+        // Print port number for registry
+        System.out.println("Server: Port number " + portNumber);        
+
+        // Create registry at port number
+        try {
+            registry = LocateRegistry.createRegistry(portNumber);
+            System.out.println("Server: Registry created on port number " + portNumber);
+        } catch (RemoteException ex) {
+            System.out.println("Server: Cannot create registry");
+            System.out.println("Server: RemoteException: " + ex.getMessage());
+            registry = null;
+        }
+
+        // Bind mEFB using registry
+//        try {
+//            registry.rebind(bindingName, mEFB);
+//        } catch (RemoteException ex) {
+//            System.out.println("Server: Cannot bind mEFB");
+//            System.out.println("Server: RemoteException: " + ex.getMessage());
+//        }
+    }
+
+    // Print IP addresses and network interfaces
+    private static void printIPAddresses() {
+        try {
+            InetAddress localhost = InetAddress.getLocalHost();
+            System.out.println("Server: IP Address: " + localhost.getHostAddress());
+            // Just in case this host has multiple IP addresses....
+            InetAddress[] allMyIps = InetAddress.getAllByName(localhost.getCanonicalHostName());
+            if (allMyIps != null && allMyIps.length > 1) {
+                System.out.println("Server: Full list of IP addresses:");
+                for (InetAddress allMyIp : allMyIps) {
+                    System.out.println("    " + allMyIp);
+                }
+            }
+        } catch (UnknownHostException ex) {
+            System.out.println("Server: Cannot get IP address of local host");
+            System.out.println("Server: UnknownHostException: " + ex.getMessage());
+        }
+
+        try {
+            System.out.println("Server: Full list of network interfaces:");
+            for (Enumeration<NetworkInterface> en = NetworkInterface.getNetworkInterfaces(); en.hasMoreElements();) {
+                NetworkInterface intf = en.nextElement();
+                System.out.println("    " + intf.getName() + " " + intf.getDisplayName());
+                for (Enumeration<InetAddress> enumIpAddr = intf.getInetAddresses(); enumIpAddr.hasMoreElements();) {
+                    System.out.println("        " + enumIpAddr.nextElement().toString());
+                }
+            }
+        } catch (SocketException ex) {
+            System.out.println("Server: Cannot retrieve network interface list");
+            System.out.println("Server: UnknownHostException: " + ex.getMessage());
+        }
+    }
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws RemoteException {
+
+        // Welcome message
+        System.out.println("SERVER USING REGISTRY");
+
+        // Print IP addresses and network interfaces
+        printIPAddresses();
+
+        // Create server
+        Server server = new Server();
+    }
 }
