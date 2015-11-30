08bee600368812a4d093d882cca88aa7c587831a
Kaspervos <KasperWoW@Live.nl>
2015-11-30 14:38:26 +0100

    Client
    Client start

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -5,10 +5,40 @@
  */
 package Client;
 
+import fontys.observer.RemotePropertyListener;
+import java.beans.PropertyChangeEvent;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 /**
  *
  * @author Kasper & Jeroen!
  */
-public class Client {
+public class Client implements RemotePropertyListener {
+
     
+    //RMI:
+    private static final int port = 1099;
+    private static final String bindName = "Test";
+    private Registry registry;
+    //todo PAS DIT AAN
+    private static final String ip = "192.168.2.11";
+    
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) throws RemoteException {
+        System.out.println("Pressed Button: " );
+    }
+    
+    
+    public Client() {
+        try {
+            registry = LocateRegistry.getRegistry(ip, port);
+        } catch (RemoteException e) {
+            System.out.println("Kan registry niet vinden: " + e.getMessage());
+        }
+        
+        System.out.println();
+    }
+
 }
