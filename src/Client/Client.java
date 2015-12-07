3e5e9f096fbabf00c3644a218cd0ce8cbacfc979
Kaspervos <KasperWoW@Live.nl>
2015-12-07 09:20:05 +0100

    Dunno g
    yo g

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -5,25 +5,31 @@
  */
 package Client;
 
+import Server.ITest;
 import fontys.observer.RemotePropertyListener;
 import java.beans.PropertyChangeEvent;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Kasper & Jeroen!
  */
-public class Client implements RemotePropertyListener {
+public class Client extends Application implements RemotePropertyListener {
 
+    private ITest test;
     
     //RMI:
     private static final int port = 1099;
     private static final String bindName = "Test";
     private Registry registry;
     //todo PAS DIT AAN
-    private static final String ip = "192.168.2.11";
+    private static final String ip = "169.254.161.102";
     
     @Override
     public void propertyChange(PropertyChangeEvent evt) throws RemoteException {
@@ -31,14 +37,35 @@ public class Client implements RemotePropertyListener {
     }
     
     
-    public Client() {
+    public Client() throws RemoteException {
+        
+        
         try {
             registry = LocateRegistry.getRegistry(ip, port);
         } catch (RemoteException e) {
             System.out.println("Kan registry niet vinden: " + e.getMessage());
         }
         
-        System.out.println();
+        try {
+            test = (ITest) registry.lookup(bindName);
+        } catch (NotBoundException e) {
+            System.out.println("Probleem bij het binden: " + e.getMessage());
+        } catch (RemoteException e) {
+            System.out.println("Probleem bij het binden: " + e.getMessage());
+        }
+        
+        System.out.println(test.getText());
+        
+    }
+    
+    public static void main(String[] args) throws RemoteException 
+    {
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+ 
     }
 
 }
